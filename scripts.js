555a60af15fb0bdfa1f7f78ba3e8044332d72d9f
dathdka <dathdka@gmail.com>
2022-09-20 20:30:14 +0700

    add and storage  note

--- scripts.js
+++ scripts.js
@@ -10,6 +10,17 @@ window.onload = () => {
         ? `I'm working`
         : `I'm taking a break`;
   });
+  chrome.storage.sync.get(["note"], (items) => {
+    var ul = document.getElementById("note");
+    console.log(items.note);
+    items.note.forEach((element, index) => {
+      var li = document.createElement("li");
+      li.id = index;
+      li.innerHTML = element;
+      li.style.color = "white";
+      ul.appendChild(li);
+    });
+  });
 };
 document.getElementById("checkbox").addEventListener("click", () => {
   if (!document.getElementById("checkbox").checked) {
@@ -45,3 +56,23 @@ document.addEventListener("DOMContentLoaded", function () {
         );
     });
 });
+
+var note = [];
+document.getElementById("inputNote").addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    var ul = document.getElementById("note");
+    chrome.storage.sync.get(["note"], (items) => {
+      var li = document.createElement("li");
+      li.id = note.length > 0 ? note.length : 0;
+      li.innerHTML = e.target.value;
+      li.style.color = "white";
+      ul.appendChild(li);
+
+      if (!items.note) note.push(e.target.value);
+      else note = [...items.note, e.target.value];
+      chrome.storage.sync.set({ note: note });
+    });
+  }
+});
+
+document.getElementById("content").addEventListener("load", () => {});
